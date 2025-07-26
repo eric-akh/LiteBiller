aafd13815c9a15a8ca868f54eeed000e7bfc6ef8
Eric <68630041+eric-akh@users.noreply.github.com>
2025-07-27 03:27:35 +1000

    A new cd including integration tests (#7)
    CI workflow includes unit tests and will be executed when making a pull request against main or production branches.
    Integration tests on the go have temporarily been disabled.

---
+++ LiteBiller.Tests/sql/setup.sql
@@ -0,0 +1,22 @@
+﻿IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Invoices' AND xtype='U')
+BEGIN
+    CREATE TABLE Invoices (
+        InvoiceId UNIQUEIDENTIFIER PRIMARY KEY,
+        InvoiceNo BIGINT IDENTITY(1,1) NOT NULL,
+        InvoiceDate DATETIME NOT NULL,
+        CustomerName NVARCHAR(255),
+        DiscountPercent DECIMAL(5,2),
+        TaxPercent DECIMAL(5,2)
+    );
+END;
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='InvoiceItems' AND xtype='U')
+BEGIN
+    CREATE TABLE InvoiceItems (
+        InvoiceItemId UNIQUEIDENTIFIER PRIMARY KEY,
+        InvoiceId UNIQUEIDENTIFIER FOREIGN KEY REFERENCES Invoices(InvoiceId),
+        Description NVARCHAR(255),
+        Quantity INT,
+        UnitPrice DECIMAL(10,2)
+    );
+END;
